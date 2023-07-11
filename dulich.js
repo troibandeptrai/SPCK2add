b4bd8cc9712126f230230239338dc1c7699fabc1
troibandeptrai <130226493+troibandeptrai@users.noreply.github.com>
2023-07-11 21:50:11 +0700

    inti

---
+++ dulich.js
@@ -0,0 +1,40 @@
+function scrollToElement1() {
+    var element = document.getElementById("#1");
+    element.scrollIntoView({ behavior: "smooth" });
+  }
+  
+  function scrollToElement2() {
+    var element = document.getElementById("#2");
+    element.scrollIntoView({ behavior: "smooth" });
+  }
+  
+  function scrollToElement3() {
+    var element = document.getElementById("#3");
+    element.scrollIntoView({ behavior: "smooth" });
+  }
+  
+  function scrollToElement4() {
+    var element = document.getElementById("#4");
+    element.scrollIntoView({ behavior: "smooth" });
+  }
+
+  function scrollToElement5() {
+    var element = document.getElementById("#5");
+    element.scrollIntoView({ behavior: "smooth" });
+  }
+
+  function scrollToElement6() {
+    var element = document.getElementById("#6");
+    element.scrollIntoView({ behavior: "smooth" });
+  }  
+
+  function scrollToElement7() {
+    var element = document.getElementById("#7");
+    element.scrollIntoView({ behavior: "smooth" });
+  } 
+
+  function scrollToElement8() {
+    var element = document.getElementById("#8");
+    element.scrollIntoView({ behavior: "smooth" });
+  }
+  
